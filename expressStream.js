aabe19834da45272f8f2070e702b0d401f771e19
Joseph Lokahi Podwys <jpodwys@ldschurch.org>
2015-07-30 12:33:13 -0600

    Adding type checking. Removing dumb stuff.

--- expressStream.js
+++ expressStream.js
@@ -2,28 +2,13 @@ var fs = require('fs');
 
 var streamBefore = [];
 var streamAfter = [];
-var openDocument = false;
-var closeHeadOpenBody = false;
-var closeDocument = false;
 
 exports.streamBefore = function(before){
-  streamBefore = before;
+  streamBefore = (typeof before === 'object' && before.length) ? before : [];
 }
 
 exports.streamAfter = function(after){
-  streamAfter = after;
-}
-
-exports.openDocument = function(val){
-  openDocument = val;
-}
-
-exports.closeHeadOpenBody = function(val){
-  closeHeadOpenBody = val;
-}
-
-exports.closeDocument = function(val){
-  closeDocument = val;
+  streamAfter = (typeof after === 'object' && after.length) ? after : [];
 }
 
 exports.stream = function(middlewareViews){
@@ -49,15 +34,8 @@ exports.stream = function(middlewareViews){
         for(var i = 0; i < streamAfter.length; i++){
           res.stream(streamAfter[i]);
         }
-        if(closeDocument){
-          res.write('</body></html>');
-        }
         this._end();
       }
-    }
-
-    if(openDocument){
-      res.write('<!DOCTYPE html><html><head>');
     }
 
     for(var i = 0; i < streamBefore.length; i++){
@@ -73,10 +51,6 @@ exports.stream = function(middlewareViews){
       else if(typeof middlewareViews === 'string'){
         res.stream(middlewareViews);
       }
-    }
-
-    if(closeHeadOpenBody){
-      res.write('</head><body>');
     }
 
     next();
